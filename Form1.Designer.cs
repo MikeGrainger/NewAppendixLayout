71e69cd578a93a9d7cec11259e366373f6857692
MikeGrainger <63049266+MikeGrainger@users.noreply.github.com>
2020-04-09 15:12:34 +0100

    Named Tabs and Refs tab with

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(APPendix));
 			this.MainGroupBox = new System.Windows.Forms.GroupBox();
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
@@ -35,8 +36,24 @@
 			this.tabPage3 = new System.Windows.Forms.TabPage();
 			this.tabPage4 = new System.Windows.Forms.TabPage();
 			this.tabPage5 = new System.Windows.Forms.TabPage();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.MainGroupBox.SuspendLayout();
 			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// MainGroupBox
@@ -68,6 +85,21 @@
 			// 
 			// tabPage1
 			// 
+			this.tabPage1.Controls.Add(this.listBox1);
+			this.tabPage1.Controls.Add(this.button5);
+			this.tabPage1.Controls.Add(this.label5);
+			this.tabPage1.Controls.Add(this.label4);
+			this.tabPage1.Controls.Add(this.button4);
+			this.tabPage1.Controls.Add(this.textBox4);
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.button3);
+			this.tabPage1.Controls.Add(this.textBox3);
+			this.tabPage1.Controls.Add(this.label2);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.textBox2);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.textBox1);
 			this.tabPage1.Location = new System.Drawing.Point(4, 22);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -116,6 +148,142 @@
 			this.tabPage5.Text = "Info:";
 			this.tabPage5.UseVisualStyleBackColor = true;
 			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(275, 17);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(74, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button1.Location = new System.Drawing.Point(247, 17);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(22, 20);
+			this.button1.TabIndex = 1;
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(152, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(89, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "BDAPP No Label";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(138, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(103, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "BDAPP Name Label";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button2.Location = new System.Drawing.Point(247, 43);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(22, 20);
+			this.button2.TabIndex = 4;
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(275, 43);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(74, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(99, 73);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(142, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "BDAPP Friendly Name Label";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// button3
+			// 
+			this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button3.Location = new System.Drawing.Point(247, 69);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(22, 20);
+			this.button3.TabIndex = 7;
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(275, 69);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(74, 20);
+			this.textBox3.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(56, 98);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(185, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "BDAPP Master Version Number Label";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// button4
+			// 
+			this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+			this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button4.Location = new System.Drawing.Point(247, 95);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(22, 20);
+			this.button4.TabIndex = 10;
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(275, 95);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(74, 20);
+			this.textBox4.TabIndex = 11;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(136, 125);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(105, 13);
+			this.label5.TabIndex = 12;
+			this.label5.Text = "BDAPP Status Label";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// button5
+			// 
+			this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+			this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button5.Location = new System.Drawing.Point(247, 121);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(22, 20);
+			this.button5.TabIndex = 13;
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(276, 124);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(73, 17);
+			this.listBox1.TabIndex = 14;
+			// 
 			// APPendix
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -130,6 +298,8 @@
 			this.Text = "APPendix";
 			this.MainGroupBox.ResumeLayout(false);
 			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -143,6 +313,21 @@
 		private System.Windows.Forms.TabPage tabPage3;
 		private System.Windows.Forms.TabPage tabPage4;
 		private System.Windows.Forms.TabPage tabPage5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
 	}
 }
 
