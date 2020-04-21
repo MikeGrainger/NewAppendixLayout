abb3f0267596659810753c6f7982d499306cc130
MikeGrainger <63049266+MikeGrainger@users.noreply.github.com>
2020-04-21 17:25:57 +0100

    sql connection class added

---
+++ MyConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace NewAppendixLayout
+{
+    public static class MyConnection
+    {
+        public static SqlConnection getConnection()
+        {
+            string conn = "Data Source = (localdb)\\ProjectsV13; Initial Catalog = APPendixDB; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            SqlConnection myConn = new SqlConnection(conn);
+            return myConn;
+        }
+    }
+}
