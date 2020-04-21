218e1f99a8aa9472fcc5e96c24c3aeba26d77690
MikeGrainger <63049266+MikeGrainger@users.noreply.github.com>
2020-04-21 17:27:40 +0100

    not sure what change i made

--- MyConnection.cs
+++ MyConnection.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Windows.Forms;
+﻿using System.Data.SqlClient;
 
 namespace NewAppendixLayout
 {
